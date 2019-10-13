2758efafbcae8775d89f769e7d8296e7672d7fa2
Ruangwit Kamnerdsap <ruangwit.k@kbtg.tech>
2019-10-13 14:09:19 +0700

    initialize project

---
+++ src/main/java/com/example/demo/SpringBootRestDemo.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestDemo {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootRestDemo.class, args);
+  }
+
+}
